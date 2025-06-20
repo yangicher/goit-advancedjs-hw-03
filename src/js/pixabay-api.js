2100bf4298cc4cbc116b3632b5a26bdfd2b5baed
Serge Iangicher <sergey.yangicher@rockyroad.io>
2025-06-20 12:16:31 +0300

    revert changes

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 const API_KEY = '50706028-1c89dce29f588a1d4e8ec62d3';
-const BASE_URL = 'https://pixabay.com/api';
+const BASE_URL = 'https://pixabay.com/api/';
 
-export function loadImages(query) {
+export async function loadImages(query) {
   const params = new URLSearchParams({
     key: API_KEY,
     q: query,
@@ -9,22 +9,12 @@ export function loadImages(query) {
     orientation: 'horizontal',
     safesearch: 'true',
   });
+  const response = await fetch(`${BASE_URL}?${params}`);
 
-  const url = `${BASE_URL}?${params}`;
-  console.log('Fetching URL:', url);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
 
-  return fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      return data.hits;
-    })
-    .catch(error => {
-      console.error('Error fetching images:', error.message || error);
-      throw error;
-    });
+  const data = await response.json();
+  return data.hits;
 }
