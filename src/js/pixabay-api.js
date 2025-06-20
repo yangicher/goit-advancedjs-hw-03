2bb6e7df6f1d86bfa56d3b668902ec56705c882c
Serge Iangicher <sergey.yangicher@rockyroad.io>
2025-06-20 11:50:21 +0300

    fix error

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -10,10 +10,10 @@ export function loadImages(query) {
     safesearch: 'true',
   });
 
-  console.log('Fetching URL: ', url);
-  console.log(`${BASE_URL}?${params}`);
+  const url = `${BASE_URL}?${params}`;
+  console.log('Fetching URL:', url);
 
-  return fetch(`${BASE_URL}?${params}`)
+  return fetch(url)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
