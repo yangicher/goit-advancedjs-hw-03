9e1f2036e9e4ea75e15e271ffe080e9bedb6963e
Serge Iangicher <sergey.yangicher@rockyroad.io>
2025-06-20 11:15:38 +0300

    fix problem with path

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,26 @@
 const API_KEY = '50706028-1c89dce29f588a1d4e8ec62d3';
-const BASE_URL = 'https://pixabay.com/api/';
+const BASE_URL = 'https://pixabay.com/api';
 
-export async function loadImages(query) {
-  const params = new URLSearchParams({
+export function loadImages(query) {
+  const searchParams = {
     key: API_KEY,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
-  });
-  const response = await fetch(`${BASE_URL}?${params}`);
+  };
 
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-
-  const data = await response.json();
-  return data.hits;
+  return axios
+    .get(BASE_URL, { params: searchParams })
+    .then(response => {
+      const { hits } = response.data;
+      if (!hits.length) {
+        throw new Error('No images found');
+      }
+      return hits;
+    })
+    .catch(error => {
+      console.error('Error loading images:', error.message || error);
+      throw error;
+    });
 }
